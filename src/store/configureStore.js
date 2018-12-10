0455eda1fe5cc6414d77213b919e22edbfc1060b
RD-Eric <eric@appmaster.cc>
2018-12-10 15:46:03 +0800

    修改設定

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,15 +1,12 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore } from 'redux';
 import Immutable from 'immutable';
-import promiseMiddleware from 'redux-promise';
-import { createLogger } from 'redux-logger';
 import rootReducer from '../reducers';
 
 const initialState = Immutable.Map();
 
 const configureStore = createStore(
   rootReducer,
-  initialState,
-  applyMiddleware(createLogger({ stateTransformer: state => state.toJS() }, promiseMiddleware))
+  initialState
 )
 
 export default configureStore;
--- webpack.build.js
+++ webpack.build.js
@@ -60,7 +60,7 @@ module.exports = env => {
     },
     plugins: [
       new webpack.DefinePlugin({
-          'process.env.NODE_ENV': JSON.stringify(env.NODE_ENV || 'development')
+          'process.env.NODE_ENV': JSON.stringify('production')
       }),
       new HtmlWebpackPlugin({
           template: './client/index.html',
