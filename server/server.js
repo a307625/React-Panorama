78fd65375dabd93b7b2a7371fc461413bfe33611
RD-Eric <eric@appmaster.cc>
2018-12-10 12:48:22 +0800

    修正favicon

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const app = new Express();
 const port = process.env.PORT || 3001
 
 
-app.get('/favicon.jpeg', function(req, res) {
+app.get('/favicon.ico', function(req, res) {
     res.status(204);
 });
 
